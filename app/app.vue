d7858dce60acb754f3fe9dc9a45a454b691dce10
constantinhacico <consatntin@norwen.nl>
2026-06-18 13:15:43 +0200

    test

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+  <div style="padding:40px;font-family:sans-serif">
+
+    <h1>🚀 Norwen Demo CI/CD Works</h1>
+
+    <p>Deployed automatically from GitHub Actions</p>
+
+    <p>{{ new Date().toISOString() }}</p>
+
   </div>
+
 </template>
